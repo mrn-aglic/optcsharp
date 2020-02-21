e79f90713d67f7ce2eee1a092a1dfa0586a20edd
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2020-02-21 10:38:41 +0100

    Deployment

--- OptLocal/Areas/Default/Controllers/TraceApiController.cs
+++ OptLocal/Areas/Default/Controllers/TraceApiController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using TracingCore;
@@ -23,24 +22,24 @@ namespace OptLocal.Areas.Default.Controllers
         }
 
         [HttpGet, Route("/api/getcsharptrace")]
-        public string GetCSharpTrace(
-            // [FromQuery] string user_script,
-            // [FromQuery] string options_json,
-            // [FromQuery] string raw_input_json
+        public JObject GetCSharpTrace(
+            [FromQuery] string user_script,
+            [FromQuery] string options_json,
+            [FromQuery] string raw_input_json
             )
         {
-            // var inputs = raw_input_json == null
-            //     ? new List<string>()
-            //     : JArray.Parse(raw_input_json).ToObject<List<string>>();
-            // var sourceRewriter = new SourceCodeRewriter(new ExpressionGenerator(), _instrumentationConfig);
-            // var optBackend = new OptBackend(user_script, inputs, sourceRewriter, _instrumentationConfig);
-            //
-            // var compilationResult = optBackend.Compile(CompilationName, true);
-            // var pyTutorData = optBackend.Trace(compilationResult.Root, compilationResult);
-            //
-            // return PyTutorDataMapper.ToJson(pyTutorData);
+            var inputs = raw_input_json == null
+                ? new List<string>()
+                : JArray.Parse(raw_input_json).ToObject<List<string>>();
+            var sourceRewriter = new SourceCodeRewriter(new ExpressionGenerator(), _instrumentationConfig);
+            var optBackend = new OptBackend(user_script, inputs, sourceRewriter, _instrumentationConfig);
+            
+            var compilationResult = optBackend.Compile(CompilationName, true);
+            var pyTutorData = optBackend.Trace(compilationResult.Root, compilationResult);
+            
+            return PyTutorDataMapper.ToJson(pyTutorData);
 
-            return "SRANJE";
+            // return "SRANJE";
         }
         
         [HttpGet, Route("/api/statuscheck")]
